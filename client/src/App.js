90ee338d7a2c4f10cc27cc43a889e114aab44c0f
gulin01 <gulin9717@gmail.com>
2022-12-05 17:01:31 +0900

    client added

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+
+const App = () => (
+  <Fragment>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </Router>
+  </Fragment>
+);
+export default App;
